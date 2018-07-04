b58e3ee92a7c5c4f9e5c2828641deb176ba795cc
Melisantux <meli.santuccio@gmail.com>
2018-07-03 21:19:35 -0300

    Resueltos ejercicios 5 y 6

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,15 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+    /* Nombre */
+    var nombre=document.getElementById("elNombre").value;
+
+    /* Edad */
+    var edad=document.getElementById("laEdad").value;
+
+    /* Datos concatenados */
+    var resultado;
+    resultado= "Usted se llama" + " " + nombre + " " + "y tiene" + " " + edad + " " + "años.";
+    alert(resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,10 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+    var primerNumero=parseInt(document.getElementById("numeroUno").value);
+    var segundoNumero=parseInt(document.getElementById("numeroDos").value);
+    var resultado;
+    resultado = primerNumero + segundoNumero;
+    alert("La suma es" + " " + resultado);
 }
 
