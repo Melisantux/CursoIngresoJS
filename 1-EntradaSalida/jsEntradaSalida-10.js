2cb9d6ade23ad13d7bdfaf53a86514f88d127b23
Melisantux <meli.santuccio@gmail.com>
2018-07-05 10:06:07 -0300

    Resueltos Ejercicios 9 y 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,11 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe;
+    importe= parseInt(document.getElementById("importe").value);
+    var descuento;
+    descuento= importe * 25 / 100;
+    var resultado;
+    resultado= importe - descuento;
+    document.getElementById("resultado").value=resultado;
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo;
+    sueldo= parseInt(document.getElementById("sueldo").value);
+    var porcentaje;
+    porcentaje= sueldo * 10 / 100;
+    var resultado;
+    resultado = sueldo + porcentaje;
+    document.getElementById("resultado").value=resultado;
 
 	
 }
