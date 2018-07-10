17057bbcd8fc4413ae0db78910af63c70bde2b21
Melisantux <meli.santuccio@gmail.com>
2018-07-10 09:15:29 -0300

    Resueltos Parciales Recuperatorio Ejercicios 1 ~ 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+    var precio;
+    precio=prompt("Ingrese el precio");
+    precio=parseInt(precio);
+    var descuento;
+    descuento=prompt("Ingrese el porcentaje del descuento");
+    descuento=parseInt(descuento);
+    var descuento$;
+    descuento$= (precio * descuento)/ 100;
+    var preciodescuento;
+    preciodescuento= precio - descuento$;
+    var iva;
+    iva = (preciodescuento * 21) / 100;
+    alert("El descuento es de $" + descuento$ + ", por lo que el precio con descuento sería $" + preciodescuento + ", más un IVA de $" + iva);
+    var preciofinal;
+    preciofinal= (precio - descuento$) + iva;
+    document.getElementById("elPrecioFinal").value= "El precio final es $" + preciofinal;
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+    var base;
+    base=prompt("Ingrese la base");
+    base = parseInt(base);
+    var altura;
+    altura=prompt("Ingrese la altura");
+    altura=parseInt(altura);
+    var perimetro;
+    perimetro= base * 3;
+    var area;
+    area= (base * altura) / 2;
+    alert("El perímetro del triángulo es " + perimetro + " y el área " + area);
 
 }
